65162ac2834f156f0f23a9bc0c8a65c671f2b5d3
Fang Pengfei <pffang@vip.qq.com>
2024-09-27 19:47:12 +0800

    Update build files

---
+++ all.cmd
@@ -0,0 +1,5 @@
+call zlib.cmd
+call bzip2.cmd
+call xz.cmd
+call zstd.cmd
+call libarchive.cmd
--- libarchive.cmd
+++ libarchive.cmd
@@ -2,6 +2,11 @@ if not exist libarchive (
     git clone --depth=1 https://github.com/libarchive/libarchive.git
 )
 
-cmake -G "Visual Studio 17 2022" -A x64 --install-prefix %~dp0stage -S libarchive -B libarchive_build -D CMAKE_BUILD_TYPE="Release" -D ZLIB_LIBRARY="%~dp0stage\lib\zlibstatic" -D ZLIB_INCLUDE_DIR=stage/include -D BZIP2_LIBRARIES=stage/lib/bz2_static.lib -D BZIP2_INCLUDE_DIR=stage/include -D LIBLZMA_LIBRARY=stage/lib/lzma.lib -D LIBLZMA_INCLUDE_DIR=stage/include -D ZSTD_LIBRARY=stage/lib/zstd_static.lib -D ZSTD_INCLUDE_DIR=stage/include
+set TEMP=%ROOT%
+rem echo %TEMP%
+SET "ROOT=%TEMP:\=/%"
+rem echo %ROOT%
+
+cmake -G "Visual Studio 17 2022" -A x64 --install-prefix %ROOT%stage -S libarchive -B libarchive_build -D CMAKE_BUILD_TYPE="Release" -D ZLIB_LIBRARY="%ROOT%stage/lib/zlibstatic.lib" -D ZLIB_INCLUDE_DIR=stage/include -D BZIP2_LIBRARIES="%ROOT%stage/lib/bz2_static.lib" -D BZIP2_INCLUDE_DIR=stage/include -D LIBLZMA_LIBRARY="%ROOT%stage/lib/lzma.lib" -D LIBLZMA_INCLUDE_DIR=stage/include -D ZSTD_LIBRARY="%ROOT%stage/lib/zstd_static.lib" -D ZSTD_INCLUDE_DIR=stage/include
 cmake --build libarchive_build --target ALL_BUILD --config Release
 cmake --build libarchive_build --target INSTALL --config Release
