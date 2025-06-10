a99e0f8e296c5a0d9f384cd2c6c61ea0c309f737
Fang Pengfei <pffang@users.noreply.github.com>
2025-06-10 19:02:01 +0800

    Revert "Fix building on dev"
    This reverts commit d13678a18cf4af5ec51b34379e30b4db98e3045b.

--- zstd.cmd
+++ zstd.cmd
@@ -2,5 +2,5 @@ if not exist zstd (
     git clone --depth=1 https://github.com/facebook/zstd.git
 )
 
-cmake -G "Visual Studio 17 2022" -A x64 --install-prefix %~dp0stage -S zstd/build/cmake -B zstd_build -D CMAKE_BUILD_TYPE="Release" -D ZSTD_BUILD_STATIC=ON -D ZSTD_BUILD_SHARED=OFF -D ZSTD_BUILD_PROGRAMS=OFF -D ZSTD_BUILD_TESTS=OFF -D ZSTD_BUILD_CONTRIB=OFF -D ZSTD_DISABLE_ASM=1
+cmake -G "Visual Studio 17 2022" -A x64 --install-prefix %~dp0stage -S zstd/build/cmake -B zstd_build -D CMAKE_BUILD_TYPE="Release" -D ZSTD_BUILD_STATIC=ON -D ZSTD_BUILD_SHARED=OFF -D ZSTD_BUILD_PROGRAMS=OFF -D ZSTD_BUILD_TESTS=OFF -D ZSTD_BUILD_CONTRIB=OFF
 cmake --build zstd_build --target INSTALL --config Release
